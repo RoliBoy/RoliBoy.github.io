9e3a72e9372dccac5d11807b2fdb08b72480e80c
roliboy <roliboy@protonmail.com>
2021-02-12 20:57:27 +0200

    add ansify and rework image slideshow javascript code

--- index.js
+++ index.js
@@ -1,164 +1,74 @@
 //TODO: make this dynamic
-slideStates = {}
-slideStates['slides-icarus'] = 0
-slideStates['slides-archer'] = 0
-// slideStates['slides-ansify'] = 0
+slideStates = [...document.getElementsByClassName('project-images')].reduce((o, k) => ({...o, [k.id]: 0}), {});
+[...document.getElementsByClassName('project-images')].forEach(div => div.children[0].style.display = 'block');
 
-function slidePrev(slideGroupClassName) {
-    slideStates[slideGroupClassName]--
-    let slides = document.getElementsByClassName(slideGroupClassName)
-    if (slideStates[slideGroupClassName] < 0)
-        slideStates[slideGroupClassName] = slides.length - 1
-    for (let slide of slides)
-        slide.style.display = 'none'
-    slides[slideStates[slideGroupClassName]].style.display = 'block'
+function slidePrev(slideGroupName) {
+  slideStates[slideGroupName]--
+  let slides = [...document.getElementById(slideGroupName).children].filter(e => e.classList.contains("project-image"))
+  if (slideStates[slideGroupName] < 0)
+    slideStates[slideGroupName] = slides.length - 1
+  for (let slide of slides)
+    slide.style.display = 'none'
+  slides[slideStates[slideGroupName]].style.display = 'block'
 }
 
-function slideNext(slideGroupClassName) {
-    slideStates[slideGroupClassName]++
-    let slides = document.getElementsByClassName(slideGroupClassName)
-    if (slideStates[slideGroupClassName] >= slides.length)
-        slideStates[slideGroupClassName] = 0
-    for (let slide of slides)
-        slide.style.display = 'none'
-    slides[slideStates[slideGroupClassName]].style.display = 'block'
+function slideNext(slideGroupName) {
+  slideStates[slideGroupName]++
+  let slides = [...document.getElementById(slideGroupName).children].filter(e => e.classList.contains("project-image"))
+  if (slideStates[slideGroupName] >= slides.length)
+    slideStates[slideGroupName] = 0
+  for (let slide of slides)
+    slide.style.display = 'none'
+  slides[slideStates[slideGroupName]].style.display = 'block'
 }
-
-document.getElementsByClassName('slides-icarus')[0].style.display = 'block'
-document.getElementsByClassName('slides-archer')[0].style.display = 'block'
-// document.getElementsByClassName('slides-ansify')[0].style.display = 'block'
-
-
 
 // var canvas = document.getElementById("canvas")
 // var renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
 
 
-// let camera, scene, stats, parameters;
-// let mouseX = 0, mouseY = 0;
+let noiseScale;
+let flowField;
 
-// let windowHalfX = window.innerWidth / 2;
-// let windowHalfY = window.innerHeight / 2;
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight)
+  canvas.parent('canvas')
 
-// const materials = [];
+  blendMode(ADD)
+  noFill()
+  stroke(255, 121, 198, 3)
+ 
+  noiseScale = 6 / width;
+  flowField = Array(floor(256 * (width / height)**2)).fill().map(_ => {return {x: random(0, width), y: random(0, height)}})
+}
 
-// init();
-// animate();
+function draw() {
+  if (window.pageYOffset > height) return
+  let angleMod = noise(frameCount *  0.000025) * TWO_PI
 
-// function init() {
-//   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
-//   camera.position.z = 1000;
+  for (let p of flowField) {
+    if (p.x < 0 || p.y < 0 || p.x > width || p.y > height) {
+      p.x = random(0, width)
+      p.y = random(0, height)
+    }
 
-//   scene = new THREE.Scene();
-//   scene.fog = new THREE.FogExp2(0x544240, 0.0008);
+    let lastx = p.x;
+    let lasty = p.y;
+    let noised = noise(p.x * noiseScale, p.y * noiseScale);
 
-//   const geometry = new THREE.BufferGeometry();
-//   const vertices = [];
+    p.x += cos(noised * TWO_PI + angleMod) * 1
+    p.y += sin(noised * TWO_PI + angleMod) * 1
 
-//   const textureLoader = new THREE.TextureLoader();
+    line(lastx, lasty, p.x, p.y)
+  }
 
-//   const sprite1 = textureLoader.load('images/covid.png');
-//   const sprite2 = textureLoader.load('images/covid.png');
-//   const sprite3 = textureLoader.load('images/covid.png');
-//   const sprite4 = textureLoader.load('images/covid.png');
-//   const sprite5 = textureLoader.load('images/covid.png');
+  if (frameCount % 60 == 0) {
+    // console.log(frameRate())
+    if (frameRate() < 50) {
+      flowField.splice(0, flowField.length / 2)
+    }
+  }
+}
 
-//   for (let i = 0; i < 2000; i++) {
-//     const x = Math.random() * 2000 - 1000;
-//     const y = Math.random() * 2000 - 1000;
-//     const z = Math.random() * 2000 - 1000;
-
-//     vertices.push(x, y, z);
-//   }
-
-//   geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-
-//   parameters = [
-//     [[1, 1, 1], sprite1, 20],
-//     [[1, 1, 1], sprite1, 15],
-//     [[1, 1, 1], sprite1, 10],
-//     [[1, 1, 1], sprite1, 8],
-//     [[1, 1, 1], sprite1, 5]
-//   ];
-
-//   for (let i = 0; i < parameters.length; i++) {
-
-//     const color = parameters[i][0];
-//     const sprite = parameters[i][1];
-//     const size = parameters[i][2];
-
-//     materials[i] = new THREE.PointsMaterial({ size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true });
-//     // materials[i] = new THREE.PointsMaterial({ size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true });
-//     // materials[i].color.setHSL(color[0], color[1], s[2]);
-
-//     const particles = new THREE.Points(geometry, materials[i]);
-
-//     particles.rotation.x = Math.random() * 6;
-//     particles.rotation.y = Math.random() * 6;
-//     particles.rotation.z = Math.random() * 6;
-
-//     scene.add(particles);
-//   }
-
-
-//   renderer.setPixelRatio(window.devicePixelRatio);
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-
-//   document.body.style.touchAction = 'none';
-//   document.body.addEventListener('pointermove', onPointerMove);
-
-//   window.addEventListener('resize', onWindowResize);
-// }
-
-// function onWindowResize() {
-//   windowHalfX = window.innerWidth / 2;
-//   windowHalfY = window.innerHeight / 2;
-
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// }
-
-// function onPointerMove(event) {
-//   if (event.isPrimary === false) return;
-
-//   mouseX = event.clientX - windowHalfX;
-//   mouseY = event.clientY - windowHalfY;
-// }
-
-
-// function animate() {
-
-//   requestAnimationFrame(animate);
-
-//   render();
-//   // stats.update();
-
-// }
-
-// function render() {
-
-//   const time = Date.now() * 0.00005;
-
-//   camera.position.x += (mouseX - camera.position.x) * 0.05;
-//   camera.position.y += (- mouseY - camera.position.y) * 0.05;
-
-//   camera.lookAt(scene.position);
-
-//   for (let i = 0; i < scene.children.length; i++) {
-//     const object = scene.children[i];
-//     if (object instanceof THREE.Points) {
-//       object.rotation.y = time * (i < 4 ? i + 1 : - (i + 1));
-//     }
-//   }
-
-//   for (let i = 0; i < materials.length; i++) {
-//     const color = parameters[i][0];
-//     // const h = (360 * (color[0] + time) % 360) / 360;
-//     const h = 1;
-//     materials[i].color.setHSL(h, color[1], color[2]);
-//   }
-//   renderer.clearDepth();
-//   renderer.render(scene, camera);
-// }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
