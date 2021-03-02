895411c701728ea90a7f95ba3b41608bf8ce3d34
roliboy <roliboy@protonmail.com>
2021-03-02 21:57:23 +0200

    update header theme and add experimental writeup page

--- index.js
+++ index.js
@@ -26,49 +26,103 @@ function slideNext(slideGroupName) {
 // var renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
 
 
-let noiseScale;
-let flowField;
+
+let objects = []
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight)
   canvas.parent('canvas')
-
-  blendMode(ADD)
+  
+  objects = new Array(32).fill().map(e => {
+    return {
+      x: random(width),
+      y: random(height),
+      r: random(TWO_PI),
+      dx: random(-0.0001, 0.0001) * width,
+      dy: random(-0.0001, 0.0001) * height,
+      dr: random(-0.001, 0.001) * PI,
+      c: random([color(255, 121, 198), color(189, 147, 249)]),
+      s: random([max(width, height) * 0.015, max(width, height) * 0.01, max(width, height) * 0.0095]),
+      t: random(['square', 'triangle'])
+    }
+  })
+  
+  strokeWeight(2)
+  strokeJoin(ROUND);
+  rectMode(RADIUS)
   noFill()
-  stroke(255, 121, 198, 3)
- 
-  noiseScale = 6 / width;
-  flowField = Array(floor(256 * (width / height)**2)).fill().map(_ => {return {x: random(0, width), y: random(0, height)}})
 }
 
 function draw() {
-  if (window.pageYOffset > height) return
-  let angleMod = noise(frameCount *  0.000025) * TWO_PI
-
-  for (let p of flowField) {
-    if (p.x < 0 || p.y < 0 || p.x > width || p.y > height) {
-      p.x = random(0, width)
-      p.y = random(0, height)
-    }
-
-    let lastx = p.x;
-    let lasty = p.y;
-    let noised = noise(p.x * noiseScale, p.y * noiseScale);
-
-    p.x += cos(noised * TWO_PI + angleMod) * 1
-    p.y += sin(noised * TWO_PI + angleMod) * 1
-
-    line(lastx, lasty, p.x, p.y)
-  }
-
-  if (frameCount % 60 == 0) {
-    // console.log(frameRate())
-    if (frameRate() < 50) {
-      flowField.splice(0, flowField.length / 2)
-    }
-  }
+  
+  clear()
+  objects.forEach(o => {
+    o.x += o.dx
+    o.y += o.dy
+    o.r += o.dr
+    if (o.x < -o.s) o.x = width + o.s
+    if (o.x > width + o.s) o.x = -o.s
+    if (o.y < -o.s) o.y = height + o.s
+    if (o.y > height) o.y = -o.s
+    push()
+    translate(o.x, o.y)
+    rotate(o.r)
+    o.c.setAlpha((1 - map(height - o.y - o.s, 0, height, 1, 0) ** 2) * 255)
+    stroke(o.c)
+    o.t == 'triangle'?
+      triangle(0, -2/3 * o.s, -1/sqrt(3) * o.s, 1/3 * o.s, 1/sqrt(3) * o.s, 1/3 * o.s):
+      rect(0, 0, o.s, o.s, o.s/2)
+    pop()
+  })
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
 }
+
+// let noiseScale;
+// let flowField;
+
+// function setup() {
+//   let canvas = createCanvas(windowWidth, windowHeight)
+//   canvas.parent('canvas')
+
+//   blendMode(ADD)
+//   noFill()
+//   stroke(255, 121, 198, 3)
+ 
+//   noiseScale = 6 / width;
+//   flowField = Array(floor(256 * (width / height)**2)).fill().map(_ => {return {x: random(0, width), y: random(0, height)}})
+// }
+
+// function draw() {
+//   if (window.pageYOffset > height) return
+//   let angleMod = noise(frameCount *  0.000025) * TWO_PI
+
+//   for (let p of flowField) {
+//     if (p.x < 0 || p.y < 0 || p.x > width || p.y > height) {
+//       p.x = random(0, width)
+//       p.y = random(0, height)
+//     }
+
+//     let lastx = p.x;
+//     let lasty = p.y;
+//     let noised = noise(p.x * noiseScale, p.y * noiseScale);
+
+//     p.x += cos(noised * TWO_PI + angleMod) * 1
+//     p.y += sin(noised * TWO_PI + angleMod) * 1
+
+//     line(lastx, lasty, p.x, p.y)
+//   }
+
+//   if (frameCount % 60 == 0) {
+//     // console.log(frameRate())
+//     if (frameRate() < 50) {
+//       flowField.splice(0, flowField.length / 2)
+//     }
+//   }
+// }
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight)
+// }
